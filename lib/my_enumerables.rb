2e62c8a375f2598d9f3fdf3aa4faf699e8e9efb7
Ugnius Vilkoncius <ugnius.vilkoncius@gmail.com>
2022-09-23 01:00:31 +0300

    Added each with index and select

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,21 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    for i in 0...self.length
+      yield self[i], i
+    end
+    self
+  end
+
+  def my_select
+    select_array = Array.new
+    for i in 0...self.length
+      if yield self[i]
+        select_array.push(self[i])
+      end
+    end
+    select_array
+  end
 end
 
 # You will first have to define my_each
